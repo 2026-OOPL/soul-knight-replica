52e430b5510cfbe6fb567560d097281bf1464ab7
YFHD-osu <yfhdtw@gmail.com>
2026-05-30 23:16:04 +0800

    Update SettingsUI.cpp

--- src/Component/UI/SettingsUI.cpp
+++ src/Component/UI/SettingsUI.cpp
@@ -26,6 +26,20 @@
 #include "Component/UI/SettingsUI.hpp"
 #include "Component/Button/ImageButton.hpp"
 #include "Util/GameObject.hpp"
+#include "Util/Input.hpp"
+
+#include <glm/vec2.hpp>
+
+#include "Component/UI/SettingsUI.hpp"
+#include "Component/Button/ImageButton.hpp"
+#include "Util/GameObject.hpp"
+#include "Util/Input.hpp"
+
+#include <glm/vec2.hpp>
+
+#include "Component/UI/SettingsUI.hpp"
+#include "Component/Button/ImageButton.hpp"
+#include "Util/GameObject.hpp"
 
 SettingsUI::SettingsUI(float zIndex)
 : Util::GameObject(nullptr, zIndex)
